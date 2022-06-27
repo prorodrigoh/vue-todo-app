fcbff034b5fd7f169c604fadf3893f0113dd6148
ROD <104014121+prorodrigoh@users.noreply.github.com>
2022-06-27 11:30:46 -0400

    Logic completed - need display output

--- src/App.vue
+++ src/App.vue
@@ -1,81 +1,106 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+// ref: handle state
+// watch: will check for any change on REF
+// onMounted: execute code as soon as the page is render, it will bring the local storage values
+// computed: will order the list by created date
+import { ref, watch, onMounted, computed } from "vue";
+
+const todos = ref([]); // will store the list of all todos
+const name = ref(""); // user name
+
+const input_content = ref(""); // the input field for the content of a todo
+const input_category = ref(null); // the input field for the category of a todo
+
+const todos_ascOrder = computed(
+  () =>
+    todos.value.sort((a, b) => {
+      return b.createdAt - a.createdAt;
+    }) // order the todos by createdAt date
+);
+
+const addTodo = () => {
+  if (input_content.value.trim() === "" || input_category.value === null) {
+    return;
+  }
+  // console.log(input_content.value.trim(), input_category.value);
+
+  // create a object inside the todos array
+  todos.value.push({
+    content: input_content.value,
+    category: input_category.value,
+    done: false,
+    createdAt: new Date().getTime(), // using miliseconds do we can order by createdAt
+  });
+};
+
+// if the variable "todos" changes on submit, we will store the newVal in the localstorage
+watch(
+  todos,
+  (newVal) => {
+    localStorage.setItem("todos", JSON.stringify(newVal)); //because it is an object we need to stringfy it
+  },
+  { deep: true } // because todos is an array, we need "deep" to look through the array
+);
+
+// if the variable "name" changes with the input, we will store the newVal in the localstorage
+watch(name, (newVal) => {
+  localStorage.setItem("name", newVal);
+});
+// we bring the value on the localStorage to the input. whenever the screen is refreshed we have it
+onMounted(() => {
+  name.value = localStorage.getItem("name") || "";
+});
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <main class="app">
+    <section class="greeting">
+      <h2 class="title">
+        What's up, <input type="text" placeholder="Name Here" v-model="name" />
+      </h2>
+    </section>
+    <section class="create-todo">
+      <h3>Create a Todo</h3>
+      <!-- Call the function addTodo to handle the submit -->
+      <form @submit.prevent="addTodo">
+        <h4>What's on your Todo list?</h4>
+        <input
+          type="text"
+          placeholder="e.g. Make a Video"
+          v-model="input_content"
+        />
+        <!-- {{ input_content }} -->
+        <h4>Select a category</h4>
+        <div class="options">
+          <label>
+            <input
+              type="radio"
+              name="category"
+              id="category1"
+              value="Business"
+              v-model="input_category"
+            />
+            <span class="bubble business"></span>
+            <div>Business</div>
+          </label>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+          <label>
+            <input
+              type="radio"
+              name="category"
+              id="category2"
+              value="Personal"
+              v-model="input_category"
+            />
+            <span class="bubble personal"></span>
+            <div>Personal</div>
+          </label>
 
-  <main>
-    <TheWelcome />
+          <!-- {{ input_category }} -->
+        </div>
+        <input type="submit" value="Add todo" />
+      </form>
+    </section>
+    <!-- {{ todos_ascOrder }} -->
   </main>
 </template>
-
-<style>
-@import './assets/base.css';
-
-#app {
-  max-width: 1280px;
-  margin: 0 auto;
-  padding: 2rem;
-
-  font-weight: normal;
-}
-
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-a,
-.green {
-  text-decoration: none;
-  color: hsla(160, 100%, 37%, 1);
-  transition: 0.4s;
-}
-
-@media (hover: hover) {
-  a:hover {
-    background-color: hsla(160, 100%, 37%, 0.2);
-  }
-}
-
-@media (min-width: 1024px) {
-  body {
-    display: flex;
-    place-items: center;
-  }
-
-  #app {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    padding: 0 2rem;
-  }
-
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-}
-</style>
